de6471834306cd54a3d5c23cf8fe656bfbacc0eb
Daniel Camilo Rodriguez Sarrias <rodriguezsarrias@gmail.com>
2026-06-04 18:41:09 -0500

    Remove basename from HashRouter for GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from '../components/Header/Header'
+import Nav from '../components/Nav/Nav'
+import Footer from '../components/Footer/Footer'
+
+import Home from '../pages/Home/Home'
+import Filtrar from '../pages/Filtrar/Filtrar'
+import Characters from '../pages/Characters/Characters'
+import CharacterDetail from '../pages/CharacterDetail/CharacterDetail'
+import Error from '../pages/Error/Error'
+
+function App() {
+  const rootStyle = {
+    width: '100%',
+    minHeight: '100vh',
+  }
+
+  const contentStyle = {
+    width: '100%',
+    flex: '1 0 auto',
+  }
+
+  return (
+    <div className="app-root" style={rootStyle}>
+      <Router>
+        <Header />
+        <Nav />
+        <main className='app-content' style={contentStyle}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/filtrar' element={<Filtrar />} />
+            <Route path='/personajes' element={<Characters />} />
+            <Route path='/personajes/:id' element={<CharacterDetail />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  )
+}
+
+export default App
